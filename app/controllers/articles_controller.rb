72c243ac258aa83a5562b0777be0aee0490e8b6a
Marvinsky <nivramsky@gmail.com>
2015-11-14 22:12:22 -0200

    14/11/15 v5

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,20 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 	end
 
+	#GET /articles/new
+	def new
+		@article = Article.new
+	end
+
+	#POST /articles
+	def create
+		@article = Article.new(title: params[:article][:title], body: params[:article][:body])
+		@article.save
+		redirect_to @article
+	end
+
+	#PUT /articles/:id
+	def update
+	end
+
 end
