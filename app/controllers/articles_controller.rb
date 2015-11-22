8954dae51e5859342d3658dfa024de546629da13
= <=>
2015-11-22 17:07:12 -0200

    22/11/15 v7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,16 @@
 class ArticlesController < ApplicationController
 	# GET /articles
 	def index
+		#todos los registros
 		@articles =	Article.all
 	end
 
 	#get /articles/:id
 	def show
+		#encuentra registro por id
 		@article = Article.find(params[:id])
+		#where no interpolar una cadena: Es inseguro
+		#Article.where(" body LIKE ?", params[:body])
 	end
 
 	#GET /articles/new
@@ -25,8 +29,17 @@ class ArticlesController < ApplicationController
 		end	
 	end
 
+
+	#DESTROY /articles/:id
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy #destroy elimina el objeto de la base de datos
+		redirect_to articles_path
+	end
+
 	#PUT /articles/:id
 	def update
+		#@article.update_attributes({tile: new title})
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 =begin
   get "/articles" index
   post "/articles" create
-  delete "/articles" delete
+  delete "/articles/:id" destroy
   get "/articles/:id" show
   get "/articles/new" new
   get "/aticles/:id/edit" edit
