b27a5cfe96aeaddbb5c522744ab3fdd2ff32423e
= <=>
2015-11-24 16:05:28 -0200

    22/11/15 v13

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+	#before_action :validate_user, except: [:show, :index]
+	before_action :authenticate_user!, except: [:show, :index]  #devise inyecta en el controlador
+	# show and index porque quiero que lean mis articulos sin haber iniciado sesion
+	before_action :set_article, except: [:index, :new, :create]
 	# GET /articles
 	def index
 		#todos los registros
@@ -8,9 +12,11 @@ class ArticlesController < ApplicationController
 	#get /articles/:id
 	def show
 		#encuentra registro por id
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 		#where no interpolar una cadena: Es inseguro
 		#Article.where(" body LIKE ?", params[:body])
+		@article.update_visits_count
+
 	end
 
 	#GET /articles/new
@@ -33,18 +39,18 @@ class ArticlesController < ApplicationController
 
 	#DESTROY /articles/:id
 	def destroy
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 		@article.destroy #destroy elimina el objeto de la base de datos
 		redirect_to articles_path
 	end
 
 	def edit
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 	end
 
 	#PUT /articles/:id
 	def update
-		@article = Article.find(params[:id])
+		#@article = Article.find(params[:id])
 		if @article.update_attributes(article_params)
 			redirect_to @article
 		else 
@@ -53,6 +59,15 @@ class ArticlesController < ApplicationController
 	end
 
 	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	#def validate_user
+	#	redirect_to new_user_session_path, notice: "Necesitas iniciar sesión"
+	#end
+
 	def article_params
 		params.require(:article).permit(:title,:body)
 	end
--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,19 @@ class Article < ActiveRecord::Base
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true, length: {minimum: 20}
 	#validates :username, format: {with: /regex/}
+	#en el caso que tengas post creados antes de la implementacion del count haz:
+	before_save :set_visits_count
+
+
+	def update_visits_count
+		self.save if self.visit_count.nil?
+		self.update(visit_count: self.visit_count + 1)
+	end
+
+
+	private
+
+	def set_visits_count
+		self.visit_count ||= 0 #el or igual sirve para asignar zero en caso el elemento es nil
+	end
 end
